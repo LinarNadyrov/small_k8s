a71f88e4c2a7587f584dfb03ceec6e1a54502c20
LinarNadyriv <linar.nadyrov@itbegin.ru>
2020-09-11 13:54:00 +0300

    added file gitignore fix4

--- vagrant/twin/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["vagrant-hostmanager",["= 1.8.9"]]],"checksum":"14852c36b991d9ff9ae3a50529a7d4cc6794a4265e902d04b053c0c6c9f086ce","vagrant_version":"2.2.9"}
